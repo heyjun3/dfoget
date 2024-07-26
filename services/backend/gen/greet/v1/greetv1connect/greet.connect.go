367da8543a91442f712a24a4235853bee54e64b3
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-07-26 23:47:20 +0900

    add init server

--- services/backend/gen/greet/v1/greetv1connect/greet.connect.go
+++ services/backend/gen/greet/v1/greetv1connect/greet.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "example/gen/greet/v1"
+	v1 "github.com/heyjun3/dforget/services/backend/gen/greet/v1"
 	http "net/http"
 	strings "strings"
 )
