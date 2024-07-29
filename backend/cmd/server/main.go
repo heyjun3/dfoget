9cc2b44eaaf09525fc569af6cc95c16ddb8cab0e
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-07-30 01:42:07 +0900

    delete greet server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,42 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"net/http"
 
-	"connectrpc.com/connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
-	greetv1 "github.com/heyjun3/dforget/backend/gen/api/greet/v1"
-	"github.com/heyjun3/dforget/backend/gen/api/greet/v1/greetv1connect"
 	"github.com/heyjun3/dforget/backend/gen/api/memo/v1/memov1connect"
 	"github.com/heyjun3/dforget/backend/server"
 )
 
-type GreetServer struct{}
-
-func (s GreetServer) Greet(
-	ctx context.Context, req *connect.Request[greetv1.GreetRequest],
-) (*connect.Response[greetv1.GreetResponse], error) {
-	log.Println("Request headers:", req.Header())
-	res := connect.NewResponse(&greetv1.GreetResponse{
-		Greeting: fmt.Sprintf("Hello, %s", req.Msg.Name),
-	})
-	res.Header().Set("Greet-Version", "v1")
-	return res, nil
-}
-
 func main() {
-	greeter := &GreetServer{}
 	memo := &server.MemoHandler{}
 	mux := http.NewServeMux()
 
-	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
-	mux.Handle(path, handler)
-	path, handler = memov1connect.NewMemoServiceHandler(memo)
+	path, handler := memov1connect.NewMemoServiceHandler(memo)
 	mux.Handle(path, handler)
 
 
