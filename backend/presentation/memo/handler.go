237eb017ea3592cd429a60ca6f35e25534989d6d
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-12-14 12:15:57 +0900

    fix tests

--- backend/presentation/memo/handler.go
+++ backend/presentation/memo/handler.go
@@ -2,25 +2,18 @@ package memo
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
-	"log/slog"
-	"net/http"
-	"net/url"
-	"strings"
 	"time"
 
 	"connectrpc.com/connect"
 
 	memoapp "github.com/heyjun3/dforget/backend/app/memo"
-	cfg "github.com/heyjun3/dforget/backend/config"
 	"github.com/heyjun3/dforget/backend/domain/memo"
 	memov1 "github.com/heyjun3/dforget/backend/gen/api/memo/v1"
 	memov1connect "github.com/heyjun3/dforget/backend/gen/api/memo/v1/memov1connect"
 	"github.com/heyjun3/dforget/backend/lib"
-	"github.com/heyjun3/dforget/backend/presentation/auth"
 	"github.com/heyjun3/dforget/backend/server"
 )
 
@@ -172,80 +165,4 @@ func (h MemoHandler) MemoStream(ctx context.Context,
 			return connect.NewError(connect.CodeInternal, fmt.Errorf("failed to send response: %w", err))
 		}
 	}
-}
-
-type OIDCHandler struct {
-	conf       cfg.Config
-	httpClient httpClient
-}
-
-type httpClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
-func NewOIDCHandler(conf cfg.Config, client httpClient) *OIDCHandler {
-	return &OIDCHandler{
-		conf:       conf,
-		httpClient: client,
-	}
-}
-
-func (h OIDCHandler) RecieveRedirect(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	slog.InfoContext(ctx, "recieve oidc redirect")
-	code := r.URL.Query().Get("code")
-	if code == "" {
-		slog.ErrorContext(ctx, "code is empty")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	formData := url.Values{
-		"grant_type":    {"authorization_code"},
-		"code":          {code},
-		"redirect_uri":  {h.conf.OIDC.RedirectUri},
-		"client_id":     {h.conf.OIDC.ClientId},
-		"client_secret": {h.conf.OIDC.ClientSecret},
-	}
-	req, err := http.NewRequest("POST", h.conf.OIDC.TokenUrl, strings.NewReader(formData.Encode()))
-	if err != nil {
-		slog.ErrorContext(ctx, err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	res, err := h.httpClient.Do(req)
-	if err != nil {
-		slog.ErrorContext(ctx, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer res.Body.Close()
-
-	buf, err := io.ReadAll(res.Body)
-	if err != nil {
-		slog.ErrorContext(ctx, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	var token OIDCToken
-	if err := json.Unmarshal(buf, &token); err != nil {
-		slog.ErrorContext(ctx, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	cookie := &http.Cookie{
-		Name:     auth.AuthCookieName,
-		Value:    token.IdToken,
-		HttpOnly: true,
-		Path:     "/",
-	}
-	http.SetCookie(w, cookie)
-
-	slog.InfoContext(ctx, "oidc verified")
-	http.Redirect(w, r, h.conf.FrontEndURL, http.StatusTemporaryRedirect)
-}
-
-type OIDCToken struct {
-	IdToken string `json:"id_token"`
 }
