2b58fd4f8ee31fbd47a3f6fa0134879c454e0977
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-12-16 10:15:28 +0900

    update

--- backend/infra/memo/repository.go
+++ backend/infra/memo/repository.go
@@ -11,6 +11,14 @@ import (
 	"github.com/uptrace/bun"
 )
 
+type MemoDM struct {
+	bun.BaseModel `bun:"table:memos"`
+	ID            uuid.UUID `bun:"id,pk,type:uuid,default:gen_random_uuid()"`
+	UserId        uuid.UUID `bun:"user_id,type:uuid,notnull"`
+	Title         string    `bun:"type:text,notnull"`
+	Text          string    `bun:"type:text,notnull"`
+}
+
 var _ memo.MemoRepositoryInterface = (*MemoRepository)(nil)
 var _ memoapp.MemoRepositoryInterface = (*MemoRepository)(nil)
 
